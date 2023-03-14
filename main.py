9110c6aa0d1a19263c4ae2a10ae03836e5c299f5
ofzlo <abcd2fghijklm@sookmyung.ac.kr>
2023-03-14 14:35:19 +0900

    feat: stt API 구현

--- main.py
+++ main.py
@@ -5,14 +5,28 @@ import sentiment_prediction
 app = Flask(__name__)
 api = Api(app, version='3.0', title='Folder API', description='Swagger 문서', doc="/api-docs")
 
-test_api = api.namespace('test', description='조회 API')
+test_api = api.namespace('STT', description='음성인식 API')
 prediction_api = api.namespace('prediction', description='감정분석 긍부정 API')
 
 
-@test_api.route('/STT2TTS')
+@test_api.route('/STT')
 class Test(Resource):
     def get(self):
-        return 'Hello World!'
+        try:
+            r = sr.Recognizer()
+
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+                try:
+                    stt = r.recognize_google(audio, language='ko-KR')
+                    return stt
+                except sr.UnknownValueError:
+                    print('오디오를 이해할 수 없습니다.')
+                except sr.RequestError as e:
+                    print(f'에러가 발생하였습니다. 에러원인 : {e}')
+
+        except KeyboardInterrupt:
+            pass
 
 
 @prediction_api.route('/<string:text>')
